e7b206ad543b4fd5b92b7729c9ae0c1ec06b7b50
mrpopopolo <popopolo@gmail.com>
2022-06-23 12:16:28 +0200

    Nouveau projet

---
+++ src/fr/isika/cda18/projet1/groupe3/fx/SampleController.java
@@ -0,0 +1,5 @@
+package fr.isika.cda18.projet1.groupe3.fx;
+
+public class SampleController {
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module ISIKA_CDA18_Groupe3_Annuaire {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens fr.isika.cda18.projet1.groupe3.fx to javafx.graphics, javafx.fxml;
+}
